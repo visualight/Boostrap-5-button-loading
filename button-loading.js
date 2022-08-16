780cd90ac0a92d7b40afec0d1bfed4f11d84afef
Raphael M <visualight@users.noreply.github.com>
2022-08-16 15:50:56 +0200

    Update button-loading.js

--- button-loading.js
+++ button-loading.js
@@ -1,3 +1,7 @@
+/******************
+ * BASIC PLUGIN
+ *****************/
+
 /**
  * Plugin jQuery button loading for Bootstrap 5
  * Usage : $(el).btn('loading'); - $(el).btn('reset');
@@ -13,6 +17,44 @@
         }
 
         if (action === 'reset' && this.data('original-text')) {
+            (conserve_btn_width?$(this).css('width', 'auto'):'');
+            this.html(this.data('original-text')).prop('disabled', false);
+        }
+    };
+}(jQuery));
+
+
+/******************
+ * ADVANCED PLUGIN
+ *****************/
+
+/**
+ * Plugin jQuery button loading for Bootstrap 5 - disable other buttons on loading (data-attribute)
+ * Usage : $(el).btn('loading'); - $(el).btn('reset');
+ * Html : <button ... data-relationals="#button2,#buttonxxx" (** accept id and class) data-loading-text="Loading..." (** accept also html like spinner icon)>My button</button>
+ * Optional param : true (conserve button width) || false (standard mode)
+ */
+(function($) {
+    $.fn.btn = function(action, conserve_btn_width=true)
+    {
+        let relationals = (this.data('relationals')?this.data('relationals').split(','):[]);
+
+        if (action === 'loading' && this.data('loading-text'))
+        {
+            $.each(relationals, function(id, el){
+                $(el).prop('disabled', true);
+            });
+
+            (conserve_btn_width?$(this).css('width', this.outerWidth()):'');
+            this.data('original-text', this.html()).html(this.data('loading-text')).prop('disabled', true);
+        }
+
+        if (action === 'reset' && this.data('original-text'))
+        {
+            $.each(relationals, function(id, el){
+                $(el).prop('disabled', false);
+            });
+
             (conserve_btn_width?$(this).css('width', 'auto'):'');
             this.html(this.data('original-text')).prop('disabled', false);
         }
